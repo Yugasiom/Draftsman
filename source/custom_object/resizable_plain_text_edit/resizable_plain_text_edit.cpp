ffaadeb0f087dc665e9f0a75ceffbc1c3e16fefc
Yugasiom <yu3ga0siomgames@gmail.com>
2025-11-01 22:45:48 +0300

    Added beautiful code, design, and several systems for the code

--- source/custom_object/resizable_plain_text_edit/resizable_plain_text_edit.cpp
+++ source/custom_object/resizable_plain_text_edit/resizable_plain_text_edit.cpp
@@ -9,6 +9,10 @@ ResizablePlainTextEdit::ResizablePlainTextEdit(QWidget *p) :
 
 
 
+    new DraftsmanHighlighter(document());
+
+
+
     lna = new QWidget(this);
     lna->setObjectName("LNA");
 
@@ -16,6 +20,10 @@ ResizablePlainTextEdit::ResizablePlainTextEdit(QWidget *p) :
 
 
     connect(this, &QTextEdit::textChanged, this, [this]() {
+        lna->update();
+    });
+
+    connect(this, &QTextEdit::cursorPositionChanged, [this]() {
         lna->update();
     });
 
@@ -50,10 +58,58 @@ void ResizablePlainTextEdit::focusInEvent(QFocusEvent *e)
     apply_font_format();
 }
 
+
+QString normalize_case(const QString &w)
+{
+    uint32_t u = 0, l = 0, i;
+    QChar ch;
+
+    for(i = 0; i < w.length(); ++i) {
+        ch = w.at(i);
+        if(ch.isUpper()) {
+            ++u;
+        } else if(ch.isLower()) {
+            ++l;
+        }
+    }
+
+
+    return u > l ? w.toUpper() : w.toLower();
+}
+
 void ResizablePlainTextEdit::keyPressEvent(QKeyEvent *e)
 {
+    if(e->modifiers() & (Qt::ControlModifier | Qt::AltModifier | Qt::MetaModifier)) {
+        QTextEdit::keyPressEvent(e);
+
+
+        return;
+    }
+
+    if(e->key() == Qt::Key_Left      || e->key() == Qt::Key_Right  ||
+       e->key() == Qt::Key_Up        || e->key() == Qt::Key_Down   ||
+       e->key() == Qt::Key_Backspace || e->key() == Qt::Key_Delete ||
+       e->key() == Qt::Key_Return    || e->key() == Qt::Key_Enter  ||
+       e->key() == Qt::Key_Tab       || e->key() == Qt::Key_Escape) {
+        QTextEdit::keyPressEvent(e);
+
+
+        return;
+    }
+
     apply_font_format();
     QTextEdit::keyPressEvent(e);
+
+    QTextCursor c = textCursor();
+    int32_t p = c.position();
+
+    c.select(QTextCursor::WordUnderCursor);
+    QString w = c.selectedText();
+    QString f = normalize_case(w);
+
+    c.insertText(f);
+    c.setPosition(p);
+    setTextCursor(c);
 }
 
 void ResizablePlainTextEdit::apply_font_format()
@@ -130,26 +186,35 @@ uint16_t ResizablePlainTextEdit::line_number_area_width() const
 
 void ResizablePlainTextEdit::paint_line_number_area(QPaintEvent *e)
 {
-    QPainter p(lna);
+    QPainter    p(lna);
     p.fillRect(e->rect(), QColor(46, 46, 46));
 
-    QFont nf;
+    QFont       nf;
     nf.setPointSize(12);
     p.setFont(nf);
 
-    QTextBlock b = document()->firstBlock();
-    int32_t bn = 1, sy = verticalScrollBar()->value();
-    QRectF r;
-    QPointF pos;
-    QString n;
+    QTextBlock  b  = document()->firstBlock();
+    int32_t     bn = 1, sy = verticalScrollBar()->value();
+    QRectF      r;
+    QPointF     pos;
+    QString     n;
+
+    QTextCursor c  = textCursor();
+    int32_t     ss = c.selectionStart(), se = c.selectionEnd(), cs = c.block().position(),
+                bs                     , be;
+    uint16_t    s;
+    QColor      nc(255, 100, 0), sc(255, 220, 0);
 
     while(b.isValid()) {
         r = document()->documentLayout()->blockBoundingRect(b);
         pos = r.topLeft() - QPointF(0, sy);
-
         if(b.isVisible()) {
+            bs = b.position();
+            be = bs + b.length();
+            s = ss < be && se > bs;
             n = QString::number(bn);
-            p.setPen(QColor(255, 100, 0));
+            (bs == cs) ? p.setPen(sc) : p.setPen(s ? sc : nc);
+
             p.drawText(0, static_cast<int32_t>(pos.y()), line_number_area_width() - 8,
                        fontMetrics().height(), Qt::AlignRight, n);
             ++bn;
--- source/custom_object/resizable_plain_text_edit/resizable_plain_text_edit.hpp
+++ source/custom_object/resizable_plain_text_edit/resizable_plain_text_edit.hpp
@@ -8,13 +8,21 @@
 #include <QFont>
 #include <QTimer>
 #include <QPainter>
+#include <QPainter>
 #include <QTextEdit>
 #include <QKeyEvent>
 #include <QTextBlock>
 #include <QScrollBar>
+#include <QScrollBar>
+#include <QTextCursor>
 #include <QTextCursor>
 #include <QResizeEvent>
+#include <QTextDocument>
+#include <QPlainTextEdit>
 #include <QTextCharFormat>
+#include <QTextCharFormat>
+#include <QRegularExpression>
+#include <QSyntaxHighlighter>
 #include <QAbstractTextDocumentLayout>
 
 
@@ -50,6 +58,69 @@ class ResizablePlainTextEdit : public QTextEdit
         void     apply_font_format     ();
         void     update_all_text_format(uint16_t);
         uint16_t calculate_font_size   ()                      const;
+};
+
+class DraftsmanHighlighter : public QSyntaxHighlighter
+{
+    public:
+        DraftsmanHighlighter(QTextDocument *p) : QSyntaxHighlighter(p)
+        {
+            QTextCharFormat f;
+
+            f.setForeground(QColor(030, 215, 000));
+            rs.append({ QRegularExpression(R"(вправо|влево|вверх|вниз|красить)",
+                                           QRegularExpression::CaseInsensitiveOption), f });
+
+            f.setForeground(QColor(255, 000, 100));
+            rs.append({ QRegularExpression(R"(цикл|если|то)",
+                                           QRegularExpression::CaseInsensitiveOption), f });
+
+            f.setForeground(QColor(000, 130, 255));
+            rs.append({ QRegularExpression(R"(\b\d+\b)",
+                                           QRegularExpression::CaseInsensitiveOption), f });
+
+            f.setForeground(QColor(150, 150, 150));
+            rs.append({ QRegularExpression(R"(:|точка)",
+                                          QRegularExpression::CaseInsensitiveOption), f });
+
+            f.setForeground(QColor(235, 220, 160));
+            rs.append({ QRegularExpression(R"(\|.*\|)",
+                                           QRegularExpression::CaseInsensitiveOption), f });
+
+            defaultFormat.setForeground(Qt::white);
+    }
+
+    protected:
+        void highlightBlock(const QString &text) override
+        {
+            setFormat(0, text.length(), defaultFormat);
+
+            uint32_t i, s, l;
+            QRegularExpressionMatchIterator it;
+            QRegularExpressionMatch m;
+            Rule r;
+
+            for(i = 0; i < rs.size(); ++i) {
+                r = rs.at(i);
+                it = r.p.globalMatch(text);
+                while(it.hasNext()) {
+                    m = it.next();
+                    s = m.capturedStart();
+                    l = m.capturedLength();
+                    setFormat(s, l, r.f);
+                }
+            }
+        }
+
+    private:
+        struct Rule
+        {
+            QRegularExpression p;
+            QTextCharFormat    f;
+        };
+
+        QVector<Rule> rs;
+        QTextCharFormat defaultFormat;
 };
 
 
